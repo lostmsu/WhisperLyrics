ee51481610003aa9c35a189367bd8f93a4b868a7
Victor Nova <lostfreeman@gmail.com>
2025-03-10 20:39:55 -0700

    produce files instead of printing lyrics to stdout

--- WhisperLyrics.cs
+++ WhisperLyrics.cs
@@ -20,6 +20,12 @@ foreach (string path in args[1..]) {
 }
 
 async Task Transcribe(FileInfo file) {
+    string destinationPath = Path.ChangeExtension(file.FullName, ".lrc");
+    if (File.Exists(destinationPath)) {
+        Console.WriteLine($"Skipping {file.Name} as {Path.GetFileName(destinationPath)} already exists.");
+        return;
+    }
+
     // This section opens the mp3 file and converts it to a wav file with 16Khz sample rate.
     using var fileStream = file.OpenRead();
 
@@ -32,8 +38,14 @@ async Task Transcribe(FileInfo file) {
     // This section sets the wavStream to the beginning of the stream. (This is required because the wavStream was written to in the previous section)
     wavStream.Seek(0, SeekOrigin.Begin);
 
+    var result = new System.Text.StringBuilder();
+
     // This section processes the audio file and prints the results (start time, end time and text) to the console.
-    await foreach (var result in thread.ProcessAsync(wavStream)) {
-        Console.WriteLine($"[{result.Start:mm\\:ss\\.ff}]{result.Text.Trim()}");
+    await foreach (var entry in thread.ProcessAsync(wavStream)) {
+        result.AppendLine($"[{entry.Start:mm\\:ss\\.ff}]{entry.Text.Trim()}");
     }
+
+    using var output = new StreamWriter(destinationPath);
+    await output.WriteAsync(result.ToString());
+    output.Close();
 }
